16245ccb90241b2456c378485ec6b2546a870f07
namhoon <frontalnh@gmail.com>
2019-03-10 11:44:49 +0900

    update

--- mysql/default.sql
+++ mysql/default.sql
@@ -1,5 +1,5 @@
 ALTER USER 'root'@'%' IDENTIFIED
-WITH mysql_native_password BY 'test1234';
+WITH mysql_native_password BY 'QmQ6syrpw8qw9JWnP3ynZ7RXZhurq4';
 SET SQL_SAFE_UPDATES = 0;
 
 CREATE DATABASE test;
