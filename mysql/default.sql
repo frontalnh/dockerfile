cf28fedf8e45af7771811e886083fc11e3f868d7
frontalnh <frontalnh@gmail.com>
2019-05-20 21:40:13 +0900

    update

--- mysql/default.sql
+++ mysql/default.sql
@@ -1,5 +1,8 @@
-ALTER USER 'root'@'%' IDENTIFIED
-WITH mysql_native_password BY 'QmQ6syrpw8qw9JWnP3ynZ7RXZhurq4';
-SET SQL_SAFE_UPDATES = 0;
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'QmQ6syrpw8qw9JWnP3ynZ7RXZhurq4';
+
+SET SQL_SAFE_UPDATES=0;
+
+FLUSH PRIVILEGES;
 
 CREATE DATABASE test;
+
